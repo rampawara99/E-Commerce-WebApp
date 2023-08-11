356a4af936c2c99833b643f95e18358357137847
Rampawara99 <ramjadhaw1999@gmail.com>
2023-08-11 11:13:11 +0530

    Remove frontend from server branch

--- frontend/src/redux/type.js
+++
@@ -1,7 +0,0 @@
-export const DELETE_ITEM = "DELETE_ITEM"; 
-export const CATEGORY_DATA = "CATEGORY_DATA";
-export const BUY_PRODUCTS = "BUY_PRODUCTS";
-export const ALERT_MESSAGE = "ALERT_MESSAGE";
-export const ADD_TO_CART_DATA = "ADD_TO_CART_DATA";
-
-
